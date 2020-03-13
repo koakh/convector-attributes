6b20eb8563d2e9133ca3cc47b01a8b2bdd2ef260
mariomonteiro <mario.monteiro@critical-links.com>
2020-03-13 21:48:18 +0000

    finished start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
+clear
+
+echo "start network"
 npm run env:restart
+
+echo "deploy chaincode"
 npm run cc:start -- person
 
+echo "invoke some stuff"
 hurl invoke person participant_register gov "Big Government" -u admin
 hurl invoke person participant_register mit "MIT" -u user1
 hurl invoke person person_create "{\"id\":\"1-100-100\", \"name\": \"Walter Montes\"}" -u admin
 
 echo 'this must fail with good error validation { Error: transaction returned with failure: {"name":"Error","status":500,"message":"No person exists with that ID pov"}'
 hurl invoke person person_get pov
+
 echo 'this must fail with good error validation { Error: transaction returned with failure: {"name":"Error","status":500,"message":"No person exists with that ID gov"}, BUT it response with Result: {"_id":"gov","_name":"Big Government","_type":"io.worldsibu.examples.participant"}'
 hurl invoke person person_get gov
 
